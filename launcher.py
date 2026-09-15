40cd598102cfedc64e50f54fa3f6fc57719430de
trufanovvet-pixel <trufanovvet@gmail.com>
2026-09-15 17:06:09 +0300

    Enable photo and screenshot medical document analysis

--- launcher.py
+++ launcher.py
@@ -1,16 +1,18 @@
 import enhanced_app2
+import media_patch
 import notification_patch
 
 
 _original_install = enhanced_app2.enhanced.install_features
 
 
-def _install_with_notification_patch(bot):
+def _install_with_patches(bot):
     _original_install(bot)
     notification_patch.install(bot)
+    media_patch.install(bot)
 
 
-enhanced_app2.enhanced.install_features = _install_with_notification_patch
+enhanced_app2.enhanced.install_features = _install_with_patches
 
 
 if __name__ == "__main__":
