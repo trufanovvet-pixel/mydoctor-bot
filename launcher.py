d5a95e6dde2eaedba4cf463e8c9de346b4e06379
trufanovvet-pixel <trufanovvet@gmail.com>
2026-09-16 19:02:43 +0300

    Enable records hub menu

--- launcher.py
+++ launcher.py
@@ -11,6 +11,7 @@ import long_message_patch
 import user_guide_patch
 import pet_records_patch
 import prevention_patch
+import records_hub_patch
 
 
 _original_install = enhanced_app2.enhanced.install_features
@@ -32,6 +33,7 @@ def _install_clean_menu_with_onboarding(bot):
     user_guide_patch.install(bot)
     pet_records_patch.install(bot)
     prevention_patch.install(bot)
+    records_hub_patch.install(bot)
     clinic_ranking_patch.install()
     web_search_patch.install(bot)
     activity_patch.install(bot)
